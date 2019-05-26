5341a5427e02f228728bad67077ec7875e1225d8
Paulo <p.felipe@msn.com>
2019-05-26 18:13:32 -0300

    First commit

---
+++ app/src/main/java/com/example/project_for_test_architecture_flient/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.project_for_test_architecture_flient
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
